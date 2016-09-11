e85d2d742f108a2eff2af3b972560111c082ad3b
Jason Rose <jrose@shutterfly.com>
2016-09-11 12:29:35 -0700

    Passing data to pug

---
+++ data.js
@@ -0,0 +1,35 @@
+var requireDirectory = require('require-directory');
+var data = requireDirectory(module, './src/data');
+exports.data = data;
+///////////////////////////////////////////////////////////////////////////////
+
+// This is an example transformation.
+var output = {};
+output.directory1 = data.directory1;
+output.directory2 = [];
+
+var dir1LookupMap = {};
+for (var dir1Key in data.directory1) {
+  if (data.directory1.hasOwnProperty(dir1Key)) {
+    var dir1Value = data.directory1[dir1Key];
+    dir1LookupMap[dir1Value.title] = dir1Value;
+  }
+}
+
+for (var dir2Key in data.directory2) {
+  if (data.directory2.hasOwnProperty(dir2Key)) {
+    var dir2Value = data.directory2[dir2Key];
+    var newSomething = [];
+
+    for (var i = 0; i < dir2Value.something.length; i++) {
+      newSomething.push(dir1LookupMap[dir2Value.something[i]]);
+    }
+
+    dir2Value.something = newSomething;
+    output.directory2.push(dir2Value);
+  }
+}
+
+console.log(output);
+
+exports.data = output;
